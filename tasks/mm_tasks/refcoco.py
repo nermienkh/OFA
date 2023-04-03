c5eb38862daec9ccf4359bab09070f31057cac02
nermienkh <nermien.khalifa@gmail.com>
2023-04-03 23:28:51 +0000

    add print for logging in _calculate_ap_score

--- tasks/mm_tasks/refcoco.py
+++ tasks/mm_tasks/refcoco.py
@@ -105,7 +105,10 @@ class RefcocoTask(OFATask):
         interacts_h = interacts[:, 3] - interacts[:, 1]
         area_interacts = interacts_w * interacts_h
         ious = area_interacts / (area_predictions + area_targets - area_interacts + 1e-6)
-        return ((ious >= thresh) & (interacts_w > 0) & (interacts_h > 0)).float()
+        print('{ious}{interacts_w}{interacts_h}'.format(ious,interacts_w,interacts_h))
+        print("flag={}".format(flag))
+        return  flag
+        #return ((ious >= thresh) & (interacts_w > 0) & (interacts_h > 0)).float()
 
     def valid_step(self, sample, model, criterion):
         loss, sample_size, logging_output = criterion(model, sample)
