2d521aced537379ba524ce8449e26671f69b42da
nermienkh <nermien.khalifa@gmail.com>
2023-04-04 00:18:07 +0000

    fix print statement

--- utils/zero_shot_utils.py
+++ utils/zero_shot_utils.py
@@ -41,7 +41,7 @@ def eval_refcoco(task, generator, models, sample, **kwargs):
         area_interacts = interacts_w * interacts_h
         ious = area_interacts / (area_predictions + area_targets - area_interacts + 1e-6)
         flag=((ious >= thresh) & (interacts_w > 0) & (interacts_h > 0)).float()
-        print('{ious}{interacts_w}{interacts_h}'.format(ious,interacts_w,interacts_h))
+        print('{} {} {}'.format(ious,interacts_w,interacts_h))
         print("flag={}".format(flag))
         return  flag
         return ((ious >= thresh) & (interacts_w > 0) & (interacts_h > 0)).float()
