701ccb94a63c54ca49808bed1d62f83b871b18f4
nermienkh <nermien.khalifa@gmail.com>
2023-04-04 00:30:52 +0000

    fix

--- tasks/mm_tasks/refcoco.py
+++ tasks/mm_tasks/refcoco.py
@@ -105,10 +105,11 @@ class RefcocoTask(OFATask):
         interacts_h = interacts[:, 3] - interacts[:, 1]
         area_interacts = interacts_w * interacts_h
         ious = area_interacts / (area_predictions + area_targets - area_interacts + 1e-6)
-        flag=((ious >= thresh) & (interacts_w > 0) & (interacts_h > 0)).float()
+        print ('refcoco.py')
+        return((ious >= thresh) & (interacts_w > 0) & (interacts_h > 0)).float()
 
-        print('{} {} {}'.format(ious,interacts_w,interacts_h))
-        print("flag={}".format(flag))
+        #print('{} {} {}'.format(ious,interacts_w,interacts_h))
+        ##print("flag={}".format(flag))
         return  flag
         #return ((ious >= thresh) & (interacts_w > 0) & (interacts_h > 0)).float()
 
--- utils/eval_utils.py
+++ utils/eval_utils.py
@@ -184,10 +184,9 @@ def eval_refcoco(task, generator, models, sample, **kwargs):
         interacts_h = interacts[:, 3] - interacts[:, 1]
         area_interacts = interacts_w * interacts_h
         ious = area_interacts / (area_predictions + area_targets - area_interacts + 1e-6)
-        flag=((ious >= thresh) & (interacts_w > 0) & (interacts_h > 0)).float()
-        print('{} {} {}'.format(ious,interacts_w,interacts_h))
-        print("flag={}".format(flag))
-        return  flag
+        print ('eval_utils.py')
+        return((ious >= thresh) & (interacts_w > 0) & (interacts_h > 0)).float()
+       
 
     gen_out = task.inference_step(generator, models, sample)
     hyps = []
